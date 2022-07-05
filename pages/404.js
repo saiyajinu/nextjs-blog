a80af0aada4bf95285d1c4d636ff8500a881a25e
unknown <adriansefcic@yahoo.ro>
2022-07-05 19:53:31 +0300

    firstt

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+// pages/404.js
+export default function Custom404() {
+    return (<div>
+                <h1>ERROR 404 - PAGE NOT FOUND</h1>
+                <h1>plm</h1>
+            </div>)
+  }
